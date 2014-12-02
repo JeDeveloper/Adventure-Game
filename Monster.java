62ae2ddbb3084aaa6daf6fd1795129ca8856e4d9
ImpressedByRocks <temour.raza@gmail.com>
2014-12-02 18:44:33 -0500

    Attack Function Created

--- Monster.java
+++ Monster.java
@@ -1,3 +1,13 @@
 public class Monster extends LivingBeing {
-  private int 
+  private String name = "GenericTroll";
+  public Monster(name, int h) {
+    super(name, h);
+  }
+  public int attack() {
+    System.out.println("GenericTroll is foaming at the mouth!");
+    System.out.println("GenericTroll used FLAME CANNON");
+    int i = (int)(Math.random() * 100);
+    System.out.println("It mildly annoyed you!")
+    return(i)
+  }
 }
