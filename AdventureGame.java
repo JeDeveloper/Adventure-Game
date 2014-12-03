f8db3bfa12755602e5b78c31f465b37a095cfc1f
ImpressedByRocks <temour.raza@gmail.com>
2014-12-03 11:18:19 -0500

    Update AdventureGame.java

--- AdventureGame.java
+++ AdventureGame.java
@@ -1,25 +1,39 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import JeXML.JeXMLInterface;
+//import JeXML.JeXMLInterface;
 
 public class AdventureGame 
 {
-	private static AdventureGame G;
-	public AdventureGame getGlobalContext(){return G;}
-	private Room m_Current;
-	
-	public static void breakall() {
-	System.out.println("GAME OVER");
-	}
-
-	public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
-	System.out.println("Hello and welcome to INTERNET FIGHTER I");
-	System.out.println("What is your name?");
-	String name = in.nextLine();
-	Player player = new Player(name, 100);
-	
-	}
-
+  private static AdventureGame G;
+  public AdventureGame getGlobalContext(){return G;}
+  private Room m_Current;
+  
+  public static boolean fightsim(Player i, Monster j) {
+    Scanner in = new Scanner(System.in);
+  
+    {
+    int pdam = i.attack();
+    int mdam = j.attack();
+    j.changeHealth(pdam);
+    i.incrementHealth(mdam);
+    if(i.getHealth() <= 0) {
+      
+      return(false);
+    }
+    else if(j.getHealth() <= 0) {
+      return(true);
+    }
+  }
+  }
+  
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Hello and welcome to INTERNET FIGHTER I");
+    System.out.println("What is your name?");
+    String name = in.nextLine();
+    Player player = new Player(name, 100);
+    
+  }
+  
 }
