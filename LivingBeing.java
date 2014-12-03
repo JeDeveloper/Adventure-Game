bf22070ce76254574d9e163ce32493e07b479633
ImpressedByRocks <temour.raza@gmail.com>
2014-12-03 11:20:18 -0500

    Update LivingBeing.java

--- LivingBeing.java
+++ LivingBeing.java
@@ -1,10 +1,11 @@
 //Done
 public abstract class LivingBeing {
   protected String name;
-  protected int hitpoints;
+  protected int health;
   boolean RoamsOnOwn;
   float moveProb;
   protected int location;
+  public Weapon weapon;
   
   
   
@@ -21,16 +22,31 @@ public abstract class LivingBeing {
     }
   }
   
+  public void weapon(Weapon w)
+  {
+    weapon = w;
+  }
+  public Weapon getWeapon()
+  {
+    return weapon;
+  }
   public void changeRoom() {
   //  
   }
   
-  }
   
-  public int health() {
-  return health;
+  /*
+  public int gHealth() {
+    return health;
+  }
+  */
+
+  public int getHealth()
+  {
+    return health;
   }
   public void changeHealth(int c) {
     health = health + c;
   }
 }
+//}
