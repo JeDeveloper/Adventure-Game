fee87821f0a2068db3fc5df278fa14b09ff0eb56
joshmshapiro <joshmshapiro@gmail.com>
2014-12-03 10:28:01 -0500

    Update Backpack.java

--- Backpack.java
+++ Backpack.java
@@ -27,6 +27,19 @@ public class Backpack extends ItemList
     i.hold = true;
   }
   
+  public Item removeItem(String s)        //trying to change holding to not holding and use the item
+  {
+    for(int c = 0; c<holding.length;c++)
+    {
+      if(holding[c].equals(s)
+      {
+        holding[c].drop;
+        holding[c].use;
+        return holding[c];
+      }
+    }
+  }
+  
   public void getItems()
   {
     for(int c = 0; c<holding.length; c++)
