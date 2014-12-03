41e7af80b899b5b7b1003081241a677fa0509ef0
ImpressedByRocks <temour.raza@gmail.com>
2014-12-03 10:19:39 -0500

    Did some shit

--- LivingBeing.java
+++ LivingBeing.java
@@ -1,13 +1,21 @@
 //Done
 public abstract class LivingBeing {
   protected String name;
-  protected int health;
+  protected int hitpoints;
+  boolean RoamsOnOwn;
+  float moveProb;
+  protected int location;
+  
   public LivingBeing(String n, int h) {
     name = n;
     health = h;
+    moveProb = Math.random();
   }
-  public void health(int h) {
-    health = h;
+  
+  
+  
+  public int health() {
+  return health;
   }
   public void changeHealth(int c) {
     health = health + c;
