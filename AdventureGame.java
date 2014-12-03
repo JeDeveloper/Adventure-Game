a5f7eb425eda365dd699efe6f7bc54ba6e71610e
joshmshapiro <joshmshapiro@gmail.com>
2014-12-03 10:13:29 -0500

    Update AdventureGame.java

--- AdventureGame.java
+++ AdventureGame.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import JeXML.JeXMLInterface;
 
@@ -8,9 +9,19 @@ public class AdventureGame
 	public AdventureGame getGlobalContext(){return G;}
 	private Room m_Current;
 	
-	public static void main(String[] args)
-	{
-		G = new AdventureGame();
+	public static void breakall() {
+	System.out.println("GAME OVER");
+	}
+
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("Hello and welcome to INTERNET FIGHTER I");
+	System.out.println("What is your name?");
+	String name = in.nextLine();
+	Player player = new Player(name, 100);
+	
+}
+}
 	}
 	
 	public AdventureGame()
