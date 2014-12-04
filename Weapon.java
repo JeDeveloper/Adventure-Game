f15a9010c0f003bce5af8bcbbbe02b5ef6c27474
joshmshapiro <joshmshapiro@gmail.com>
2014-12-04 13:37:16 -0500

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -5,7 +5,7 @@ public class Weapon extends Item
 {
   protected int damage;
   protected int poison;//Why?
-  protected String name;
+  
   private String[] types = new String[5]; //What *is* this?
   public static final String TypeName = "WEAPON"; //For XML. Do not touch.
  
@@ -16,17 +16,17 @@ public class Weapon extends Item
     types[2] = "axe";
     types[3] = "stick";
     types[4] = "large rock"; //Can be in the declaration
-    name = types[(int)Math.random()*5];
+    name = types[(int)Math.random()*5] + damage;
     damage = (int)Math.random()*15+1;
     poison = (int)Math.random()*5;
   }
   
   public Weapon(JeXMLNode node)
   {
-	  super(node);
-	  name = node.getChildNodeContentByName("Name");
-	  damage = Integer.parseInt(node.getChildNodeContentByName("iDamage"));
-	  poison = Integer.parseInt(node.getChildNodeContentByName("iPoison"));
+   super(node);
+   name = node.getChildNodeContentByName("Name");
+   damage = Integer.parseInt(node.getChildNodeContentByName("iDamage"));
+   poison = Integer.parseInt(node.getChildNodeContentByName("iPoison"));
   }
   
   public int getDamage()
@@ -49,13 +49,13 @@ public class Weapon extends Item
   @Override
   public JeXMLNode getXML()
   {
-	  JeXMLNode base = super.getXML();
-	  base.addChildNode(new JeXMLNode("Name", name));
-	  base.addChildNode(new JeXMLNode("Type", TypeName));
-	  base.addChildNode(new JeXMLNode("iDamage", ""+damage));
-	  base.addChildNode(new JeXMLNode("iPoison"));
-	  //Not sure how to deal with 'types' since I have no idea what it means.
-	  return base;
+   JeXMLNode base = super.getXML();
+   base.addChildNode(new JeXMLNode("Name", name));
+   base.addChildNode(new JeXMLNode("Type", TypeName));
+   base.addChildNode(new JeXMLNode("iDamage", ""+damage));
+   base.addChildNode(new JeXMLNode("iPoison"));
+   //Not sure how to deal with 'types' since I have no idea what it means.
+   return base;
   }
 }
   
