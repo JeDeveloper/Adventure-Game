1acdc13609889b78a8ceb10abf1593cbc00d2c05
ImpressedByRocks <temour.raza@gmail.com>
2014-12-07 17:09:00 -0500

    Complete
    Action.java is a static function set.  It contains fightsim(Player,Monster) and drinkPotion(Player,Potion)

--- Action.java
+++ Action.java
@@ -1,4 +1,23 @@
 public class Action 
 {
-
+ public static boolean fightsim(Player i, Monster j) {
+    Scanner in = new Scanner(System.in);
+  
+    {
+    int pdam = i.attack();
+    int mdam = j.attack();
+    j.changeHealth(pdam);
+    i.incrementHealth(mdam);
+    if(i.getHealth() <= 0) {
+      
+      return(false);
+    }
+    else if(j.getHealth() <= 0) {
+      return(true);
+    }
+  }
+  public static String drinkPotion(Player a, Potion b) {
+    a.incrementHealth(b.getStrength());
+    return("After drinking a potion, " + a.toString());
+  }
 }
